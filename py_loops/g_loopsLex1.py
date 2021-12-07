9a9cdf4acccfdf111bb677c5d1897033196522f0
Grant <geleazar@gmail.com>
2021-12-07 15:35:47 -0800

    loops

---
+++ py_loops/g_loopsLex1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+#start list
+for i in range(1, 50):
+    if i == 13:
+        continue
+    print(i)
+    if i == 39:
+        break
+
+
+   
---
+++ py_loops/g_loops_ex2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# using a while loop
+i=0
+while i <= 49:
+    if i == 13:
+        i+=1
+        continue
+    print(i)
+    i+=1
+
