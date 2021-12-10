61f948028cac1d2704fe5df54ca7e0bad1b45408
Grant <geleazar@gmail.com>
2021-12-10 07:14:22 -0800

    day 4 rest

---
+++ day4/rest/restapi_ex1.py
@@ -0,0 +1,26 @@
+#!/bin/usr/env python
+import requests
+from rich import print
+
+from urllib3.exceptions import InsecureRequestWarning
+
+#Ignore insecure warnings for sites with self-signed certificates
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+def main():
+    url = "https://netbox.lasthop.io/api/"
+    http_headers = {"accept": "application/json; version=2.4;"}
+    response = requests.get(url, headers=http_headers, verify=False)
+
+    print()
+    print(dir(response))
+    print(response.text)
+    print()
+    print(response.json())
+    print()
+    print(response.ok)
+    print()
+    print(response.status_code)
+
+if __name__ == "__main__":
+    main()
